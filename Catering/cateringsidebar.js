8b263adaf6120c4619fc6478b89fe756042b906a
Raees Arshad <Raees.Arshad@tibercreek.com>
2026-05-10 15:35:59 -0400

    added scrol to the menu item when you click sidebar menu

--- Catering/cateringsidebar.js
+++ Catering/cateringsidebar.js
@@ -1,17 +1,19 @@
-document.querySelectorAll(".catering-sidebar button").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const target = btn.getAttribute("data-target");
-    const section = document.getElementById(target);
+function initSidebarScroll() {
+  document.querySelectorAll(".catering-sidebar button").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const target = btn.getAttribute("data-target");
+      const section = document.getElementById(target);
 
-    if (!section) return;
+      if (!section) return;
 
-    const yOffset = -80; // adjust if header height changes
-    const y =
-      section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      const yOffset = -10;
+      const y =
+        section.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
-    window.scrollTo({
-      top: y,
-      behavior: "smooth",
+      window.scrollTo({
+        top: y,
+        behavior: "smooth",
+      });
     });
   });
-});
+}
